ac8a266105da8791d7ee65cfc1e3d8437d0a7ee8
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2024-04-28 13:10:43 +0530

    completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -144,40 +144,7 @@ const Header = () => {
           <div className="row">
             <div className="col-12">
               <div className="menu-bottom d-flex align-items-center gap-30">
-                <div>
-                  <div className="dropdown">
-                    <button
-                      className="btn btn-secondary dropdown-toggle bg-transparent border-0 gap-15 d-flex align-items-center"
-                      type="button"
-                      id="dropdownMenuButton1"
-                      data-bs-toggle="dropdown"
-                      aria-expanded="false"
-                    >
-                      <img src="/images/menu.svg" alt="menu" />
-                      <span className="me-5 d-lnline-block">Shop Categories</span>
-                    </button>
-                    <ul
-                      className="dropdown-menu"
-                      aria-labelledby="dropdownMenuButton1"
-                    >
-                      <li>
-                        <Link className="dropdown-item text-white" to="#">
-                          Action
-                        </Link>
-                      </li>
-                      <li>
-                        <Link className="dropdown-item text-white" to="#">
-                          Another action
-                        </Link>
-                      </li>
-                      <li>
-                        <Link className="dropdown-item text-white" to="#">
-                          Something else here
-                        </Link>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
+                
                 <div className="menu-links">
                   <div className="d-flex align-items-center gap-15">
                     <NavLink className="text-white" to="/">
