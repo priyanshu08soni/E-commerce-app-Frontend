c8fe3b16b1f205eed8a46b9451417e92112ec9dc
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2024-05-22 12:31:36 +0530

    hello

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -2,6 +2,8 @@ const getTokenFromLocalStorage=localStorage.getItem("customer")?localStorage.get
 export const config={
     headers:{
         Authorization:`Bearer ${getTokenFromLocalStorage!==null?getTokenFromLocalStorage.token:""}`,
-        Accept:"application/json"
+        Accept:"application/json",
+        "Access-Control-Allow-Origin": "https://e-commerce-app-frontend-pink.vercel.app",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
     }
 }
