b9a20013267292bcafce4d8c26473ba5bcc59b94
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2024-05-22 15:00:27 +0530

    ererer

--- src/features/blogs/blogService.js
+++ src/features/blogs/blogService.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 import {base_url} from "../../utils/base_url";
-import { config } from "../../utils/axiosConfig";
 
 const getBlogs=async()=>{
-    const response=await axios.get(`${base_url}blog/`,config);
+    const response=await axios.get(`${base_url}blog/`);
     return response.data;
 }
 const getABlog=async(id)=>{
--- src/features/products/productService.js
+++ src/features/products/productService.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 import {base_url} from "../../utils/base_url"
-import { config } from "../../utils/axiosConfig";
 
 const getProducts =async(data)=>{
-    const response=await axios.get(`${base_url}product?${data?.brand?`brand=${data?.brand}&&`:""}${data?.tag?`tags=${data?.tag}&&`:""}${data?.category?`category=${data?.category}&&`:""}${data?.minPrice?`price[gte]=${data?.minPrice}&&`:""}${data?.maxPrice?`price[lte]=${data?.maxPrice}&&`:""}${data?.sort?`sort=${data?.sort}&&`:""}`,config);
+    const response=await axios.get(`${base_url}product?${data?.brand?`brand=${data?.brand}&&`:""}${data?.tag?`tags=${data?.tag}&&`:""}${data?.category?`category=${data?.category}&&`:""}${data?.minPrice?`price[gte]=${data?.minPrice}&&`:""}${data?.maxPrice?`price[lte]=${data?.maxPrice}&&`:""}${data?.sort?`sort=${data?.sort}&&`:""}`);
     return response.data;
 }
 const getAProduct=async(id)=>{
