b7ed0414a316f79772dc8c5cb9efb61bcf05a078
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2024-05-22 09:31:23 +0530

    qqqqqqqqqq

--- src/features/products/productService.js
+++ src/features/products/productService.js
@@ -1,12 +1,17 @@
 import axios from "axios";
 import {base_url} from "../../utils/base_url"
-
+const config = {
+    headers: {
+      "Access-Control-Allow-Origin": "https://e-commerce-app-frontend-pink.vercel.app",
+      "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+    }
+};
 const getProducts =async(data)=>{
-    const response=await axios.get(`${base_url}product?${data?.brand?`brand=${data?.brand}&&`:""}${data?.tag?`tags=${data?.tag}&&`:""}${data?.category?`category=${data?.category}&&`:""}${data?.minPrice?`price[gte]=${data?.minPrice}&&`:""}${data?.maxPrice?`price[lte]=${data?.maxPrice}&&`:""}${data?.sort?`sort=${data?.sort}&&`:""}`);
+    const response=await axios.get(`${base_url}product?${data?.brand?`brand=${data?.brand}&&`:""}${data?.tag?`tags=${data?.tag}&&`:""}${data?.category?`category=${data?.category}&&`:""}${data?.minPrice?`price[gte]=${data?.minPrice}&&`:""}${data?.maxPrice?`price[lte]=${data?.maxPrice}&&`:""}${data?.sort?`sort=${data?.sort}&&`:""}`,config);
     return response.data;
 }
 const getAProduct=async(id)=>{
-    const response=await axios.get(`${base_url}product/${id}`);
+    const response=await axios.get(`${base_url}product/${id}`,config);
     return response.data;
 }
 const rateProduct=async(data)=>{
