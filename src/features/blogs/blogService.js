e445c35c4aea59bcfa9a492ef0b4bc74783e9c4e
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2024-05-22 12:15:24 +0530

    asdasdasdasd

--- src/features/blogs/blogService.js
+++ src/features/blogs/blogService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import {base_url} from "../../utils/base_url";
 
 const getBlogs=async()=>{
-    const response=await axios.get(`${base_url}blog/`);
+    const response=await axios.get(`${base_url}blog`);
     return response.data;
 }
 const getABlog=async(id)=>{
