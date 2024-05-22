564d1164a145a0b73d440de19e18739b02478a30
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2024-05-22 09:59:59 +0530

    ch

--- src/features/blogs/blogService.js
+++ src/features/blogs/blogService.js
@@ -1,17 +1,12 @@
 import axios from "axios";
 import {base_url} from "../../utils/base_url";
-const config = {
-    headers: {
-      "Access-Control-Allow-Origin": "https://e-commerce-app-frontend-pink.vercel.app",
-      "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
-    }
-};
+
 const getBlogs=async()=>{
-    const response=await axios.get(`${base_url}blog/`,config);
+    const response=await axios.get(`${base_url}blog/`);
     return response.data;
 }
 const getABlog=async(id)=>{
-    const response=await axios.get(`${base_url}blog/${id}`,config);
+    const response=await axios.get(`${base_url}blog/${id}`);
     return response.data;
 }
 export const blogService={
