e50b552d00bb06c6881c941f80dcaa7a6de0cdfb
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2024-05-22 12:25:41 +0530

    sddsdsdsdsd

--- src/features/products/productService.js
+++ src/features/products/productService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import {base_url} from "../../utils/base_url"
 
 const getProducts =async(data)=>{
-    const response=await axios.get(`${base_url}product/${data?.brand?`brand=${data?.brand}&&`:""}${data?.tag?`tags=${data?.tag}&&`:""}${data?.category?`category=${data?.category}&&`:""}${data?.minPrice?`price[gte]=${data?.minPrice}&&`:""}${data?.maxPrice?`price[lte]=${data?.maxPrice}&&`:""}${data?.sort?`sort=${data?.sort}&&`:""}`);
+    const response=await axios.get(`${base_url}product?${data?.brand?`brand=${data?.brand}&&`:""}${data?.tag?`tags=${data?.tag}&&`:""}${data?.category?`category=${data?.category}&&`:""}${data?.minPrice?`price[gte]=${data?.minPrice}&&`:""}${data?.maxPrice?`price[lte]=${data?.maxPrice}&&`:""}${data?.sort?`sort=${data?.sort}&&`:""}`);
     return response.data;
 }
 const getAProduct=async(id)=>{
